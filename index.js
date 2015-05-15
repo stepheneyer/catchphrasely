683db51c215aaf7f43d0dd587fc0e5d471677f77
“Stephen <stephen@stepheneyer.comssh-keygen -t rsa -C stephen@stepheneyer.com>
2015-05-15 08:44:44 -0700

    Form Post and Delete Working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//SERVER-SIDE
+// SERVER-SIDE
 
 // REQUIREMENTS //
 var express = require("express"),
@@ -10,10 +10,10 @@ var app = express();
 
 // CONFIG //
 
-//Serve js & css files into a public folder
+// Serve js & css files into a public folder
 app.use (express.static(__dirname + '/public'));
 
-//Body parser config
+// Body parser config
 app.use (bodyParser.urlencoded({extended: true}));
 
 // DATA //
@@ -31,28 +31,42 @@ var phrases = [
 // ROUTES //
 
 // root path
-
 app.get("/", function (req, res){
 	// render index.html
 	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 //phrases index path
-
 app.get("/phrases", function (req, res){
-	//render phrases as JSON
+	// render phrases as JSON
 	res.send(JSON.stringify(phrases));
 });
 
 //delete a phrase
-
 app.post("/phrases", function (req, res){
-	//phrase#delete
+	var newPhrase = req.body;
+	// add a unique id
+	newPhrase.id = phrases[phrases.length - 1].id + 1;
+	// add new phrase to the array
+	phrases.push(newPhrase);
+	// send a reponse with newly created object
+	res.send(JSON.stringify(newPhrase));
+});
 
+app.delete("/phrases/:id", function (req, res){
+  	// set the value of the id
+  	var targetId = parseInt(req.params.id, 10);  //what is 10?
+  	// find item in the array matching the id
+  	var targetItem = _.findWhere(phrases, {id: targetId});
+  	// get the index of the found item
+  	var index = phrases.indexOf(targetItem);
+  	// remove the item at that index, only remove 1 item
+  	phrases.splice(index, 1);
+  	// render deleted object
+  	res.send(JSON.stringify(targetItem));
 });
 
 //listen on port 3000
-
 app.listen(3000, function() {
 	console.log("Running");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,62 @@
 //CLIENT-SIDE
 
-
 // on page load
 $(function() {
     // get and render the phrase
-    Phrases.all();
+    Phrase.all();
+    // set the view's behaviors
+    View.init();
 });
 
-// View Object
-
+// VIEW OBJECT
 function View() {}
+View.init = function() {
+    // phrase form submit event listener
+    $("#phrase-form").on("submit", function(e) {
+        // stop page reload
+        e.preventDefault();
+        // format form data into a query string
+        var phraseParams = $(this).serialize();
+        Phrase.create(phraseParams);
+    });
+};
+
 View.render = function(items, parentId, templateId) {
     // render a template
     var template = _.template($("#" + templateId).html());
     // input data into template and append to parent
-    $("#" + parentId).html(template({
-        collection: items
-    }));
+    $("#" + parentId).html(template({collection: items}));
 };
 
-//Phrases Object
-
-function Phrases() {}
-Phrases.all = function() {
+// PHRASES OBJECT
+function Phrase() {}
+Phrase.all = function() {
     $.get("/phrases", function(res) {
-        //parse the response
+        // parse the response
         var phrases = JSON.parse(res);
-        //render the results
-        View.render(phrases, "phrases-ul", "phrases-template");
+        // render the results
+        View.render(phrases, "phrase-ul", "phrases-template");
+    });
+};
+
+Phrase.create = function(phraseParams) {
+    $.post("/phrases", phraseParams).done(function(res) {
+        // append new phrase to the page
+        Phrase.all();
+        //reset form
+        $("#phrase-form")[0].reset();
+    });
+};
+
+
+Phrase.delete = function(phrase) {
+    var phraseId = $(phrase).data().id;
+    $.ajax({
+        url: '/phrases/' + phraseId,
+        type: 'DELETE',
+        success: function(res) {
+            //once successful, re-render all phrases
+            Phrase.all();
+        }
     });
 };
